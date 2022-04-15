237ef06e0a9f4bfeea3d206aa5db341ebfa5d0d9
DenisShip <90905485+DenisShip@users.noreply.github.com>
2022-04-15 16:57:35 +0300

    commit

---
+++ src/main/java/ru/gb/shipil/HomeWorkApp.java
@@ -0,0 +1,21 @@
+package ru.gb.shipil;
+
+public class HomeWorkApp {
+    public static void main(String[] args) {
+        printThereWords();
+        checkSumSign();
+
+
+    }
+
+    public static void printThereWords (){
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    public static void checkSumSign(){
+        int a = 4;
+        int b = 5;
+    }
+}
